0b0f7b5d49e2f8cf42b5472c5a3df354a9d93f7e
Ethan B <ethantbeauregard@gmail.com>
2020-08-03 22:34:52 -0500

    Updates

---
+++ precache-manifest.7e450348bdcedc6a50ed4256274e890d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802bfe25f88aecbcf49becb4edeecc7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3745b8f1fd8c73a24f83",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "43c3f33df3a069d984ba",
+    "url": "/static/js/2.4391a4a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4391a4a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3745b8f1fd8c73a24f83",
+    "url": "/static/js/main.4a1f0895.chunk.js"
+  },
+  {
+    "revision": "89ee7b59d64399cea10c",
+    "url": "/static/js/runtime-main.ca8602c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
